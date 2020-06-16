4cb8676a3dd72c9828f458cea93e07bc7302e66e
Fatoumata A Diaby <fatimad5293@gmail.com>
2020-06-16 05:12:24 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -10,9 +10,7 @@ class Dancer
     @name = name
   end
 
- extend 
-  FancyDance::ClassMethods
-  include 
-   FancyDance::InstanceMethods
+ extend FancyDance::ClassMethods
+ include FancyDance::InstanceMethods
 
 end
