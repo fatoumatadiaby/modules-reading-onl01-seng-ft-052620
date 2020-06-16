9b3ffe9dd3ea1736655f5df39865a137e5963baa
Fatoumata A Diaby <fatimad5293@gmail.com>
2020-06-16 04:59:04 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,12 +1,3 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
-class Dancer 
-  include Dance 
-   attr_accessor :name
- 
-  def initialize(name)
-    @name = name
-  end
- 
-end 
